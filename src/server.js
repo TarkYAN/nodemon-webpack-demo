6762495ce424e36818be0123d6f12b36b306f5f8
Austin Willoughby <arwigm@rit.edu>
2024-08-20 12:12:34 -0400

    Fixed eslint errors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,4 @@
 const http = require('http');
-const url = require('url');
-const query = require('querystring');
 const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
 
